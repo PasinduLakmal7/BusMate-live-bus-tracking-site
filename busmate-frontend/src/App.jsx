43f65de397db88618bdc7e9f69f16d3b62d736cd
savindu-st <savindu606@gmail.com>
2026-02-12 22:27:46 +0530

    refractor the routing and navigation

--- busmate-frontend/src/App.jsx
+++ busmate-frontend/src/App.jsx
@@ -1,17 +1,12 @@
 import React from "react";
-import ToggleColorMode from "./components/ToggleColorMode.jsx";
-import Views from "./components/Views.jsx";
-import LiveDriverLocation from "./components/LiveDriverLocation";
-import LiveBusMap from "./components/LiveBusMap";
-
+import Navbar from "./components/Navbar";
+import Views from "./components/Views";
 
 const App = () => {
   return (
     <>
-      <Views/>
-      <ToggleColorMode/>
-      <LiveDriverLocation driverId="driver_001" />
-      <LiveBusMap driverId="driver_001" />
+      <Navbar />
+      <Views />
     </>
   );
 };
