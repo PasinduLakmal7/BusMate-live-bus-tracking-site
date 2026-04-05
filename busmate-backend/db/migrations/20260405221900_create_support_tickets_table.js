068954cd0d8145b787987680dc66ea1c9e944180
Pasindu Lakmal <pherath119@gmail.com>
2026-04-05 23:12:54 +0530

    help tble

---
+++ busmate-backend/db/migrations/20260405221900_create_support_tickets_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('support_tickets', (table) => {
+    table.increments('ticket_id').primary();
+    table.string('topic', 100).notNullable();
+    table.string('full_name', 150).notNullable();
+    table.text('message').notNullable();
+    table.string('status', 50).defaultTo('OPEN');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('support_tickets');
+};
